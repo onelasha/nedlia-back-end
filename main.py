4eb99823ff93045270d5adf6104b7306b0908225
Lavrenti DeLavrenti <one.lasha@gmail.com>
2024-11-30 10:50:19 -0800

    Fix 001 (#3)
    * adding main
    
    * fixing
    
    * Fix-003
    
    * Adding comment
    
    ---------
    
    Co-authored-by: Lavrenti DeLavrenti <3443132+onelasha@users.noreply.github.com>

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ versions = ["1.14.56", "1.15.0", "2.0.0", "1.14.55", "1.14.50", "1.13.99"]
 for version in versions:
     is_valid = bool(re.match(REGEX, version))
     print(f"{version}: {is_valid}")
+# This is a comment
 # if __name__ == "__main__":
 #     pass
