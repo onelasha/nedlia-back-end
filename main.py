2eb9afd9dd4c5323521f5a46c43fa6d2edeace88
Lavrenti DeLavrenti <one.lasha@gmail.com>
2024-11-30 12:19:59 -0800

    Adding code coverage (#7)
    * .
    
    * adding tests
    
    * Fixing pipeline
    
    * Fix pipeline
    
    * .
    
    * .
    
    * .
    
    ---------
    
    Co-authored-by: Lavrenti DeLavrenti <3443132+onelasha@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def add(a, b):
     return a + b
 
 
-if __name__ == "__main__":
-    # Example usage
-    print("This is the main script!")
-    result = add(2, 3)
-    print(f"2 + 3 = {result}")
+# if __name__ == "__main__":
+#     # Example usage
+#     print("This is the main script!")
+#     result = add(2, 3)
+#     print(f"2 + 3 = {result}")
