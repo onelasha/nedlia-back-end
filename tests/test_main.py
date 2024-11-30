ea6c4d5d014471706d81c3f71e44e4f21a05bf3e
Lavrenti DeLavrenti <one.lasha@gmail.com>
2024-11-30 11:42:10 -0800

    Unite test (#4)
    * adding main
    
    * unit test
    
    * env and pytes
    
    * Adding new file
    
    ---------
    
    Co-authored-by: Lavrenti DeLavrenti <3443132+onelasha@users.noreply.github.com>

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from main import add
+
+
+def test_add():
+    """
+    Test the add function with sample inputs.
+    """
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
