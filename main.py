4a7fea2eabbec11fa50b614c10d1c76bca9277d7
Lavrenti DeLavrenti <one.lasha@gmail.com>
2024-12-07 10:57:07 -0800

    Update main.py (#10)

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-"""
-This module provides a simple greeting function.
-"""
+from fastapi import FastAPI
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+import asyncio
+import uvloop  # Add this import
 
 
-def add(a, b):
-    """
-    Simple function to add two numbers.
-    """
-    return a + b
+app = FastAPI()
 
+def doSomething(arg: int) -> None:
+    print("hello trg_21.11")
 
-# if __name__ == "__main__":
-#     # Example usage
-#     print("This is the main script!")
-#     result = add(2, 3)
-#     print(f"2 + 3 = {result}")
+def hello_world() -> None:
+    print("hello world")
+
+async def start_server():
+    config = Config()
+    config.bind = ["0.0.0.0:80"]
+    config.use_reloader = True
+    # config.debug = True
+    config.worker_class = "uvloop"
+    config.accesslog = "-"    
+    await serve(app, config)
+
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())  # Add this line
+    asyncio.run(start_server())
