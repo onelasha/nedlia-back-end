ce78e9ccb0b0789a1848a0499fffa9b48fa3f6e1
Lavrenti DeLavrenti <one.lasha@gmail.com>
2025-02-09 10:46:13 -0800

    sonar_mod2

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -1,8 +1,13 @@
 """Tests for main.py"""
 
+from unittest import TestCase
+
 from app.__main__ import main
 
 
-def test_main():
-    """Test that main() runs without errors"""
-    main()
+class TestMain(TestCase):
+    """Test cases for main.py"""
+
+    def test_main(self):
+        """Test that main() runs without errors"""
+        main()
